125b972f4b92e73f0760ab28cad423ab8ccd8669
Kacper Kula <kulak@kulak.at>
2023-05-20 22:26:17 +0100

    MPE moved around.

--- src/mpe/index.tsx
+++ src/mpe/index.tsx
@@ -1,8 +1,43 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./app";
-import "./style.css"
+import { render } from 'solid-js/web';
+import { MidivalInputs } from './components/midival-inputs/midival-inputs';
+import { Show, createSignal } from 'solid-js';
+import { IMIDIAccess, IMIDIInput, IMIDIOutput, MIDIVal, MIDIValOutput } from '@midival/core';
+import { InputDashboard } from './components/inputDashboard/input-dashboard';
+import { MidivalOutputs } from './components/midival-outputs/midival-outputs';
+import { OutputDashboard } from './components/outputDashboard/outputDashboard';
+import "./style.module.css"
 
-const container = document.getElementById("app");
-const root = createRoot(container!)
-root.render(<App />);
+function HelloWorld() {
+    const [access, setAccess] = createSignal<IMIDIAccess>()
+    const [input, setInput] = createSignal<IMIDIInput>()
+    const [output, setOutput] = createSignal<IMIDIOutput>()
+
+    MIDIVal.connect()
+    .then(a => setAccess(() => a))
+
+    const inputSelected = (input: IMIDIInput) => {
+        setInput(() => input)
+    }
+
+    const outputSelected = (output: IMIDIOutput) => {
+        setOutput(() => output)
+    }
+        
+    return <div>
+        <Show when={access() && !input() && !output()}>
+            <div>Connect to following inputs to see MPE messages being sent:</div>
+            <MidivalInputs access={access} onSelect={inputSelected} />
+
+            <div>Connect to following outputs to start sending MPE messages:</div>
+            <MidivalOutputs access={access} onSelect={outputSelected} />
+        </Show>
+        <Show when={input()}>
+            <InputDashboard input={input()} />
+        </Show>
+        <Show when={output()}>
+            <OutputDashboard output={output()} />
+        </Show>
+        </div>;
+    }
+
+render(() => <HelloWorld />, document.getElementById('app'))
